420133acc53a6ad6551f1ecbe72bab96a7b7fd23
romeritocampos <Romerito@10.7.192.177>
2014-08-06 15:33:44 -0300

    Method insert has been implemented.

--- cassandradb/src/main/java/org/cassandradb/CassandraConnection.java
+++ cassandradb/src/main/java/org/cassandradb/CassandraConnection.java
@@ -1,5 +1,6 @@
 package org.cassandradb;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import com.datastax.driver.core.Cluster;
@@ -31,13 +32,46 @@ public class CassandraConnection {
 
 	public void insert (String columnName, Map<String, String> data) {
 		
+		String insert = "INSERT INTO ";
+		String columns = " (";
+		String info = " (";
+		
+		for (String key : data.keySet()) {
+			columns += key + ",";
+			info += data.get(key) + ",";	
+		}
+		
+		columns = columns.subSequence(0, columns.length()-1) + ") ";
+		info = info.substring(0, info.length()-1) + ");";
+		
+		insert += columnName + columns + " VALUES " + info;
+		
+		session.execute(insert);
 		
 	}
 	
 	public Map<String, String> select (String columnName) {
 		
 		
+		
 		return null;
+	}
+	
+	public static void main (String[] args) {
+		
+		
+		CassandraConnection cc = new CassandraConnection("127.0.0.1");
+		
+		cc.connect("logap");
+		
+		Map<String, String> data = new LinkedHashMap<String, String>();
+		data.put("id","2");
+		data.put("name", "'fusca'");
+		
+		cc.insert("car", data);
+		
+		
+		
 	}
 	
 }
