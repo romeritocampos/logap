2a8ea3f479366693f4659b741a05bcce17051596
romeritocampos <Romerito@10.7.192.177>
2014-08-06 14:29:34 -0300

    Initial commit.

---
+++ cassandradb/src/main/java/org/cassandradb/App.java
@@ -0,0 +1,13 @@
+package org.cassandradb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
