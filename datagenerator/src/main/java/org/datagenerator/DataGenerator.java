966bd45add458c884486228cd9a18dec5c34955e
romeritocampos <Romerito@10.7.192.177>
2014-08-06 17:00:11 -0300

    Constructor of DataGenerator created.

--- datagenerator/src/main/java/org/datagenerator/DataGenerator.java
+++ datagenerator/src/main/java/org/datagenerator/DataGenerator.java
@@ -1,7 +1,18 @@
 package org.datagenerator;
 
+import org.cassandradb.CassandraConnection;
+
 public class DataGenerator {
 
+	private CassandraConnection connection;	
+	private static int id = 0;
+	
+	public DataGenerator(String contactPoint) {
+		
+		connection = new CassandraConnection(contactPoint);
+		
+	}
+	
 	
 	
 }
