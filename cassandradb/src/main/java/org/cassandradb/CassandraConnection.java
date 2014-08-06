a8843048c8b01e39223632b9d111eeea67545745
romeritocampos <Romerito@10.7.192.177>
2014-08-06 15:07:40 -0300

    Methods added to CassandraConnection class.
    Constructor of CassandraConnection implemented.

--- cassandradb/src/main/java/org/cassandradb/CassandraConnection.java
+++ cassandradb/src/main/java/org/cassandradb/CassandraConnection.java
@@ -1,7 +1,38 @@
 package org.cassandradb;
 
+import java.util.Map;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
 public class CassandraConnection {
 
+	private Cluster cluster;
+	private Session session;
 	
+	public CassandraConnection(String contactPoint) {
+		
+		cluster = Cluster.builder().addContactPoint(contactPoint).build();
+		
+	}
+	
+	public void connect () {
+		
+	}
+	
+	public void close () {
+		
+	}
+
+	public void insert (String columnName, Map<String, String> data) {
+		
+		
+	}
+	
+	public Map<String, String> select (String columnName) {
+		
+		
+		return null;
+	}
 	
 }
